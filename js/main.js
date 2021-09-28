35dcecd7b952da20050fbed996c92392e8cf25a6
chihab1991 <chihab.chergui.1991@gmail.com>
2021-09-28 10:19:21 +0100

    add sticky plan select header

--- js/main.js
+++ js/main.js
@@ -7,15 +7,3 @@ const close = (targetedDiv, btn) => {
 };
 
 close(specialOffer, closeSpecialOffer);
-
-const info1 = document.querySelector(".info1");
-const info2 = document.querySelector(".info2");
-const info3 = document.querySelector(".info3");
-const info4 = document.querySelector(".info4");
-const info5 = document.querySelector(".info5");
-
-message1 = "";
-message2 = "";
-message3 = "";
-message4 = "";
-message5 = "";
